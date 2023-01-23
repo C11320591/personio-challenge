24ce040096e5a58d95c536cec2917d4cfc880fdc
C11320591 <dmur@amazon.com>
2023-01-23 23:26:54 +0000

    Populating missing definitions in constants file

---
+++ cdk/lib/constants/constants.ts
@@ -0,0 +1,29 @@
+export const APP_NAME = "personio-challenge";
+
+export const STACK_ENVIRONMENTS = {
+    "dev": {
+        account: "249422412389",
+        region: "eu-west-1"
+    },
+    "prod": {
+        "prod-wave-1": {
+            account: "249422412389",
+            region: "eu-west-1"
+        }
+    }
+};
+
+/*
+ * Webserver attributes are defined here and referenced
+ * in manifests.ts to ensure definition consistency.
+ */
+export const WEBSERVER_ATTRIBUTES = {
+    ECR_IMAGE: "249422412389.dkr.ecr.eu-west-1.amazonaws.com/dmur-challenge:latest",
+    ENVIRONMENT_VARIABLES: [
+        {
+            name: "HELLO",
+            value: "Hello"
+        }
+    ],
+    PORTS_CONFIG: { protocol: "TCP", port: 80, targetPort: 8080 }
+};
