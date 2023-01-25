8a1f6ec63dc96127b147339ff8bdce73ace2d080
C11320591 <dmur@amazon.com>
2023-01-25 19:24:44 +0000

    'APP_NAME' backticks not required since we're not appending to the string

--- cdk/lib/manifests/manifests.ts
+++ cdk/lib/manifests/manifests.ts
@@ -3,19 +3,19 @@ import { APP_NAME, WEBSERVER_ATTRIBUTES } from '../constants/constants';
 export const deploymentManifest = {
     apiVersion: "apps/v1",
     kind: "Deployment",
-    metadata: { name: `${APP_NAME}` },
+    metadata: { name: APP_NAME },
     spec: {
         replicas: 12,
         selector: {
-            matchLabels: { app: `${APP_NAME}` }
+            matchLabels: { app: APP_NAME }
         },
         template: {
             metadata: {
-                labels: { app: `${APP_NAME}` }
+                labels: { app: APP_NAME }
             },
             spec: {
                 containers: [{
-                    name: `${APP_NAME}`,
+                    name: APP_NAME,
                     image: WEBSERVER_ATTRIBUTES.ECR_IMAGE,
                     ports: [{ containerPort: WEBSERVER_ATTRIBUTES.PORTS_CONFIG.port }],
                     env: WEBSERVER_ATTRIBUTES.ENVIRONMENT_VARIABLES,
@@ -29,10 +29,10 @@ export const deploymentManifest = {
 export const serviceManifest = {
     apiVersion: "v1",
     kind: "Service",
-    metadata: { name: `${APP_NAME}` },
+    metadata: { name: APP_NAME },
     spec: {
         type: "LoadBalancer",
-        selector: { app: `${APP_NAME}` },
+        selector: { app: APP_NAME },
         ports: [WEBSERVER_ATTRIBUTES.PORTS_CONFIG]
     }
 };
