e06b36023f2eb6eb8922f7fe8b74141b15f031f9
C11320591 <dmur@amazon.com>
2023-01-25 20:35:45 +0000

    Removing 'AlbController' for now -- can be included if we switch from using classic ELB

--- cdk/lib/personio-webserver-stack.ts
+++ cdk/lib/personio-webserver-stack.ts
@@ -1,6 +1,6 @@
 import { Construct } from 'constructs';
 import { Stack, StackProps } from 'aws-cdk-lib';
-import { Cluster, KubernetesVersion, AlbControllerVersion } from 'aws-cdk-lib/aws-eks';
+import { Cluster, KubernetesVersion } from 'aws-cdk-lib/aws-eks';
 import { APP_NAME } from './constants/constants';
 import { deploymentManifest, serviceManifest } from './manifests/manifests';
 
@@ -13,7 +13,6 @@ export class personioWebserverStack extends Stack {
 
         const cluster: Cluster = new Cluster(this, clusterName, {
             version: KubernetesVersion.V1_21,
-            albController: { version: AlbControllerVersion.V2_2_4 },
             defaultCapacity: Stack.of(this).availabilityZones.length
         });
 
